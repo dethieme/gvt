845df7fa92a35b7cd61c0df4a50e4ca6c37d7c48
denise.thieme <de.thieme@ostfalia.de>
2025-02-03 20:08:46 +0100

    Redesign; added sprite sheet animation

---
+++ script.js
@@ -0,0 +1,37 @@
+const disc = document.getElementById('disc');
+const frames = 24; // 0° to 345° in 15° steps
+let currentFrame = 0;
+let autoRotate = false;
+
+document.addEventListener('keydown', (event) => {
+    if (event.repeat === false) {
+        if (event.key === 'l') {
+            updateFrame('left');
+        } else if (event.key === 'r') {
+            updateFrame('right');
+        } else if (event.key === 'a') {
+            autoRotate = !autoRotate;
+
+            if (autoRotate) {
+                startAutoRotation();
+            }
+        }
+    }
+});
+
+function updateFrame(direction) {
+    if (direction === 'left') {
+        currentFrame = (currentFrame - 1 + frames) % frames;
+    } else if (direction === 'right') {
+        currentFrame = (currentFrame + 1) % frames;
+    }
+
+    disc.src = `assets/frame_${currentFrame}.png`;
+}
+
+function startAutoRotation() {
+    if (autoRotate) {
+        updateFrame('right');
+        setTimeout(startAutoRotation, 40);
+    }
+}
